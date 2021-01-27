232c2c8c6597ba62176aa6a832f86c98fd9c1f02
klimenkoff9 <dennis.klimenkov9@gmail.com>
2021-01-27 12:00:02 -0500

    Hash password

--- api/user.js
+++ api/user.js
@@ -1,28 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const { User } = require('../db/models');
-const bcrypt = require('bcrypt');
 
 
 // Route paths are prepended with /api/user/
 router.post("/", async (req, res, next) => {
-	try {
-    let { email, password } = req.body;
-    let hashedPassword = await bcrypt.hash(password, 10);
-    const users = await User.create({
-      email: email,
-      password: hashedPassword
-    })
-    User.prototype.validPassword = async function(password) {
-      return await bcrypt.compare(password, this.password);
-  } 
-  console.log(await bcrypt.compare(password, users.password));
-    res.status(200).json(users);
-	} catch (error) {
-		console.error(error);
-		res.status(500).json({ message: "Can't read request" });
-		next(error);
-	}
+  try {
+  const user = User.findOne({
+    where: { email: req.body.email}
+  })
+  } catch (error) {
+  
+  }
 });
 
 // Export our router, so that it can be imported to construct our api routes
