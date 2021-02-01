16fd00faa99faf68bc59f35c0d628bfd63aa0124
jahedev <jahedev@gmail.com>
2021-01-31 20:00:54 -0500

    heroku deploy attempt #2

--- db/db.js
+++ db/db.js
@@ -3,20 +3,15 @@ require('dotenv').config()
 
 // Initialize database with Sequelize
 
-const db = new Sequelize(
-  process.env.DATABASE_URL,
-  process.env.DATABASE_USER,
-  process.env.DATABASE_PASS,
-  {
-    dialect: 'postgres',
-    protocol: 'postgres',
-    dialectOptions: {
-      ssl: {
-        sslmode: 'require',
-        rejectUnauthorized: false,
-      },
+const db = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      sslmode: 'require',
+      rejectUnauthorized: false,
     },
-  }
-)
+  },
+})
 
 module.exports = db
