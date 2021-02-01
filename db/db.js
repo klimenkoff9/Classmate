810ee9008de3bf528346d41313e6834626d198a4
jahedev <jahedev@gmail.com>
2021-01-31 19:54:33 -0500

    set up db.js for heroku

--- db/db.js
+++ db/db.js
@@ -1,14 +1,22 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const Sequelize = require('sequelize')
+require('dotenv').config()
 
 // Initialize database with Sequelize
 
-const db = new Sequelize(process.env.DATABASE_URL || process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASS, {
-  host : "localhost",
-  dialect: "postgres",
-  logging: false
-});
+const db = new Sequelize(
+  process.env.DATABASE_URL,
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASS,
+  {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        sslmode: 'require',
+        rejectUnauthorized: false,
+      },
+    },
+  }
+)
 
-module.exports = db;
-
-
+module.exports = db
